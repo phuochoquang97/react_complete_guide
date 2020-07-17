f20e88374d929b9d96776a0c312f06026f1c0b8b
phuochoquang97 <56500909+phuochoquang97@users.noreply.github.com>
2020-07-17 14:10:54 +0700

    adding two way binding

--- src/App.js
+++ src/App.js
@@ -11,14 +11,24 @@ class App extends Component {
       {name: 'stephanie', age: 20}
     ]
   }
-  switchNameHander = () => {
+  switchNameHander = (newName) => {
     //console.log('Was click');
     //Dont do this: this.state.persons[0].name = 'max';
     this.setState({
       persons: [
-        {name: 'maximum', age: 28},
+        {name: newName, age: 28},
         {name: 'manu', age: 29},
         {name: 'stephanie', age: 27}
+      ]
+    })
+  }
+
+  nameChangedHandler=(event)=>{
+    this.setState({
+      persons: [
+        {name: 'max', age: 28},
+        {name: event.target.value, age: 29},
+        {name: 'stephanie', age: 26}
       ]
     })
   }
@@ -27,10 +37,18 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working</p>
-        <button onClick={this.switchNameHander}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
+        <button onClick={()=>this.switchNameHander('maximum')}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}></Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHander.bind(this, 'max')}
+          changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}></Person>
       </div>
     );
     //return React.createElement('div', {className: 'App'} , React.createElement('h1',null,'Hi, I\'m a React App!!!'));
